67e84a71c607091f0d70b6128bc6612c52ca8b27
wlan0ether <mwangijesse929@gmail.com>
2023-07-15 07:24:56 -0700

    perfect

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -7,8 +7,8 @@
 */
 int main() {
 
-	for(int i = 0; i <= 9; i++){
-		printf("The value, %d is less than 10%d\n", i)
+	for (int i = 0; i <= 9; i++){
+		printf("The value, %d is less than 10\n", i);
 	}
 
 	return 0;
