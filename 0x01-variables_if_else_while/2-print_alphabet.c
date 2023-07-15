663f0a54d812dc06e4aeb5927e434dc2bc50689d
wlan0ether <mwangijesse929@gmail.com>
2023-07-14 18:03:37 -0700

    printing the alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: 0 on success, non-zero on failure
+ */
+int main(void)
+{
+	for (char i = "a"; i <= "z"; i++)
+	{
+		putchar(i);
+	}
+	putchar("\n");
+
+	return 0;
+
+}
