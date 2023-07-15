42e39c6ea8adcf1033bd612fa2191c69fcbdd18d
wlan0ether <mwangijesse929@gmail.com>
2023-07-15 07:18:58 -0700

    printing zero to nine integers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - program entry
+ *
+ * Return: 0 (success)
+*/
+int main() {
+
+	for(int i = 0; i <= 9; i++){
+		printf("The value, %d is less than 10%d\n", i)
+	}
+
+	return 0;
+
+}
