96a98bf4031775f3a42ce6f02fbbeeb4670accd2
wlan0ether <mwangijesse929@gmail.com>
2023-07-18 15:19:44 -0700

    printing the underscore putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * main - print _putchar
+ *
+ * Return: (0)
+ *
+ */
+int main(void)
+{
+	int i;
+	char c[] = "_putchar";
+
+	for (i = 0; i <= 7; i++)
+	{
+		_putchar(c[i]);
+	}
+
+	_putchar('\n');
+
+	return (0);
+}
