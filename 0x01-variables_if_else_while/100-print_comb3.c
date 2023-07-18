522d55147c08668015f26447cd0eca251aafacd5
wlan0ether <mwangijesse929@gmail.com>
2023-07-17 17:57:39 -0700

    fabulous

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - program entry
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int firstdigit = '0';
+	int seconddigit = '0';
+
+	for (seconddigit = '0'; seconddigit <= '9'; seconddigit++)
+	{
+		for (firstdigit = '0'; firstdigit <= '9'; firstdigit++)
+		{
+			if(!((firstdigit == seconddigit) || (seconddigit > firstdigit)))
+			{
+				putchar(seconddigit);
+				putchar(firstdigit);
+				if (!(firstdigit == '9' && seconddigit == '8'))
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
