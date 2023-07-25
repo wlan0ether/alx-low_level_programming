e7d637ca0cdde0a58d8525d07002319ae5fb054e
root <root@kali>
2023-07-25 03:39:02 +0300

    perfect

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,29 @@
+#include "main"
+/**
+ * puts2 - function should print only one character out of two
+ * starting with the first one
+ * @string: input
+ * Return: print
+ */
+void puts2(char *str)
+{
+	int longi =0;
+	int t = 0;
+	char *y = str;
+	int o;
+
+	while (*y != '\0')
+	{
+		y++;
+		longi++;
+	}
+	t = longi -1;
+	for (o = 0; o <= t; 0++)
+	{
+		if (o % 2 == 0)
+	{
+		putchar(str[o]);
+	}
+	}
+	putchar('\n');
+}
