62c784e110bba07538564322af5d8638de598a63
root <root@kali>
2023-07-25 03:46:43 +0300

    perfect perfect yes yes

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -2,12 +2,12 @@
 /**
  * puts2 - function should print only one character out of two
  * starting with the first one
- * @string: input
+ * @str: input
  * Return: print
  */
 void puts2(char *str)
 {
-	int longi =0;
+	int longi = 0;
 	int t = 0;
 	char *y = str;
 	int o;
@@ -17,7 +17,7 @@ void puts2(char *str)
 		y++;
 		longi++;
 	}
-	t = longi -1;
+	t = longi - 1;
 	for (o = 0; o <= t; o++)
 	{
 		if (o % 2 == 0)
