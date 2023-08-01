5cd73de6d9363d2d7493e87ae58bf895794ba708
root <root@kali.root>
2023-07-31 21:53:21 -0400

    number two six

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * *_memset - fills memory with a constant byte
+ * _memset - fills memory with a constant byte
  * @s: pointer to the memory area to fill
  * @b: the constant byte to fill the memory with
  * @n: number of bytes to fill
@@ -10,11 +10,12 @@
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int i = 0;
+	int i = 0;
 
-	for (i = 0; i < n; i++)
+	for (; n > 0; i++)
 	{
 		s[i] = b;
+		n--;
 	}
 
 	return s;
