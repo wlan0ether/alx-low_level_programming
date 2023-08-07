4e199667d5024bd05d62407623d781b9dc3ceb4d
root <root@kali.root>
2023-08-07 05:53:59 -0400

    yes no

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -8,7 +8,7 @@
  *
  * Return: Always 0 (Success)
  */
-int main (int argc _attribute_((unused)), char *argv[])
+int main (int __attribute__((unused))argc, char *argv[])
 {
 	printf("%s\n", *argv);
 	return (0);
